1db34074e5f6f7e433df8a04ca7c20c6b866871c
Keelavijayabhaskar <vijaykeela25@gmail.com>
2026-07-13 13:27:37 +0530

    files updated

--- app.py
+++ app.py
@@ -326,6 +326,8 @@ def predict():
         crop_touchpoints=crop_info["touchpoints"],
     )
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
